d0a4b83495404a8b85eb2a8e92c27cf12d70ffb6
CHARLIE MELBOURNE <cm257889@falmouth.ac.uk>
2022-11-07 14:51:41 +0000

    Removing trees is now clean with no tilemap tearing

--- Assets/Scripts/Axe.cs
+++ Assets/Scripts/Axe.cs
@@ -7,7 +7,6 @@ public class Axe : MonoBehaviour
 {
     public PlaceTiles tileScript;
     public Tilemap currentTilemap;
-    public Tile yay;
     void Start()
     {
         tileScript = gameObject.GetComponent<PlaceTiles>();
@@ -17,7 +16,23 @@ public class Axe : MonoBehaviour
         tileScript.treeGrid[tile.x,tile.y].isTree = false;
         if (tileScript.grid1[tile.x,tile.y].isWater == true) { currentTilemap = tileScript.trees; }
         else { currentTilemap = tileScript.upperTrees; }
-        currentTilemap.SetTile(new Vector3Int(tile.x,tile.y,1), null);
-        //currentTilemap.SetTile()
+        
+        tileScript.trees.SetTile(new Vector3Int(tile.x,tile.y,1), null);
+        tileScript.upperTrees.SetTile(new Vector3Int(tile.x, tile.y, 1), null);
+        currentTilemap.SetTile(new Vector3Int(tile.x, tile.y, 1), null);
+
+        if (tileScript.treeGrid[tile.x,tile.y + 1].isTree == true)
+        {
+            currentTilemap.SetTile(new Vector3Int(tile.x, tile.y + 1, 1), tileScript.forestTiles[0]);
+        }
+        else
+        {
+            tileScript.trees.SetTile(new Vector3Int(tile.x, tile.y + 1, 1), null);
+            tileScript.upperTrees.SetTile(new Vector3Int(tile.x, tile.y + 1, 1), null);
+        }
+        if (tileScript.treeGrid[tile.x, tile.y - 1].isTree == true)
+        {
+            currentTilemap.SetTile(new Vector3Int(tile.x, tile.y, 1), tileScript.forestTiles[1]);
+        }
     }
 }
