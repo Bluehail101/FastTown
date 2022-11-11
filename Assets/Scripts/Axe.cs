e1180d1225a6be53c23f3af7502f808debf0ab40
Charlie Melbourne <cm257889@falmouth.ac.uk>
2022-11-11 15:21:22 +0000

    Was working on Axe, but a fire alarm happened...

--- Assets/Scripts/Axe.cs
+++ Assets/Scripts/Axe.cs
@@ -7,6 +7,7 @@ public class Axe : MonoBehaviour
 {
     public PlaceTiles tileScript;
     public Tilemap currentTilemap;
+    public Resource resourceScript;
     void Start()
     {
         tileScript = gameObject.GetComponent<PlaceTiles>();
@@ -14,25 +15,30 @@ public class Axe : MonoBehaviour
     public void removeTree(Vector3Int tile)
     {
         tileScript.treeGrid[tile.x,tile.y].isTree = false;
+        //Set the tree grid tile to not be a tree anymore.
         if (tileScript.grid1[tile.x,tile.y].isWater == true) { currentTilemap = tileScript.trees; }
         else { currentTilemap = tileScript.upperTrees; }
+        //Determines whether or not the tree is on the lower or upper level.
         
         tileScript.trees.SetTile(new Vector3Int(tile.x,tile.y,1), null);
         tileScript.upperTrees.SetTile(new Vector3Int(tile.x, tile.y, 1), null);
-        currentTilemap.SetTile(new Vector3Int(tile.x, tile.y, 1), null);
+        //Currently removes tree tile from both layers due to an error that can occur here.
 
         if (tileScript.treeGrid[tile.x,tile.y + 1].isTree == true)
         {
             currentTilemap.SetTile(new Vector3Int(tile.x, tile.y + 1, 1), tileScript.forestTiles[0]);
+            //If tile above the selected is also tree, switch it's tile to a bottom tree tile.
         }
         else
         {
             tileScript.trees.SetTile(new Vector3Int(tile.x, tile.y + 1, 1), null);
             tileScript.upperTrees.SetTile(new Vector3Int(tile.x, tile.y + 1, 1), null);
+            //If not, remove the above tile too, to remove the top part of the tree.
         }
         if (tileScript.treeGrid[tile.x, tile.y - 1].isTree == true)
         {
             currentTilemap.SetTile(new Vector3Int(tile.x, tile.y, 1), tileScript.forestTiles[1]);
+            //If the tile
         }
     }
 }
